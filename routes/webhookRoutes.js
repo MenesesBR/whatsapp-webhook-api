1d7aa05bc3d64a333b241047fec97a6a76981ff1
João Victor Meneses <joao.vms22@hotmail.com>
2025-04-25 17:32:43 -0300

    fixed handleMessage import

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const webhookVerifier = require('../controllers/webhookVerifier');
-const messageHandler = require('../controllers/messageHandler');
+const { handleMessage } = require('../controllers/messageHandler');
 
 router.get('/webhook', webhookVerifier);
-router.post('/webhook', messageHandler);
+router.post('/webhook', handleMessage);
 
 module.exports = router;
