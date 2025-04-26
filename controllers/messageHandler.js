ac0c5376c26e4feafbd9b864bf15df6e224f9673
João Victor Meneses <joao.vms22@hotmail.com>
2025-04-26 18:44:52 -0300

    fixed issues

--- controllers/messageHandler.js
+++ controllers/messageHandler.js
@@ -9,9 +9,9 @@ async function handleMessage(req, res) {
     const message = value?.messages?.[0];
 
     if (!message || !message?.from) {
-      return res.sendStatus(201);
+      return res.sendStatus(201).json({ status: 'ok' });
     }
-    res.sendStatus(200);
+    res.sendStatus(200).json({ status: 'ok' });
 
 
     const userPhoneNumber = message.from;
